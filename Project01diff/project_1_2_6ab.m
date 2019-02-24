4de3b6e0db58356ba20d5a1c8226f9c3caa8ed7e
Lilac Intrater <liin7533@colorado.edu>
2019-02-24 16:55:24 -0700

    Question 6 a and b on one plot

---
+++ Project01diff/project_1_2_6ab.m
@@ -0,0 +1,21 @@
+
+
+r = .65;
+L = 8.1;
+p = 1.2;
+q = 1;
+h = @(x) p*x.^2./(q+x.^2);
+x_prime = @(t,x) r*x.*(1-x/L)-h(x);
+h1 = .01;
+t1 = 0:h1:75;
+x2 = 0:h1:75;
+x1 = zeros(1,length(t1));
+x1(1) = 2;
+for n = 1:(length(t1)-1)
+    x1(n+1) = x1(n) + h1*x_prime(t1(n),x1(n));
+end
+f = @(t,x) (0.65*(1-(x./8.1)).*x)-(1.2*x.^2./(1+x.^2));
+hold on
+plot(t1,x1)
+dirfield(f,0:1:30, 0:0.5:15, 'name')
+hold off
