a84697dd2a80e49ca1a2ec5471060bb7536831b3
Lilac Intrater <liin7533@colorado.edu>
2019-02-28 18:31:00 -0700

    more changes

--- Project01diff/flow.m
+++ Project01diff/flow.m
@@ -1,6 +1,6 @@
 close all; clear all; 
 % The following code is from flow.m
-
+%%
 % Generating vector field for Lotka-Volterra System of equations
 x1min = -1; x1max = 6; x2min = -1; x2max = 6;
 x1step = .2; x2step = .2; 
@@ -15,6 +15,21 @@ axis([x1min x1max x2min x2max])
 
 % Calling project_system_3_1_5.m for ode45 to find solutions with initial
 % conditions x1=0.5 and x2=1
+%{
+function vprime = project_system_3_1_5(t, v)
+    x1 = v(1);
+    x2 = v(2);
+    a = 1.5;
+    b = 1.1;
+    c = 2.5;
+    d = 1.4;
+    
+    vprime = zeros(2, 1);
+    
+    vprime(1) = -a*x1+(b*x1.*x2);
+    vprime(2) = c*x2-(d*x1.*x2);
+end
+%}
 [t_out, v_out] = ode45(@project_system_3_1_5, [0,20], [0.5,1]);
 figure(1)
 hold on
@@ -25,6 +40,7 @@ hold on
 plot(c/d,a/b, 'g.', 'MarkerSize', 20);
 plot(0,0, 'g.', 'MarkerSize', 20);
 
+% Adding nullclines and equilibirum points
 e = c/d;
 n1 = refline([0 a/b]); n2 = refline([0 0]);
 n1.Color = 'k'; n2.Color = 'k';
@@ -35,40 +51,14 @@ n3.LineWidth = 1; n4.LineWidth = 1;
 n3.Color = 'r'; n4.Color = 'r';
 plot(v_out(:,1), v_out(:,2))
 hold off
-
-% ANSWER TO QUESTION 1:
-% 1st order, autonomous, linear
-
-% ANSWER TO QUESTION 2: 
-% Nullclines: x1 = 0, x2 = a/b and x1 = c/d, x2 = 0
-% Equilibrium Solutions: (0,0), (c/d, a/b)
-
-% ANSWER TO QUESTION 3:
-% Figure 1
-
-% ANSWER TO QUESTION 4: 
-% (0,0) is semi stable, (c/d, a/b) is unstable
-
-% ANSWER TO QUESTION 5 (along with figure 1)
-% The solution does at we expected, because it will never reach an
-% equilibrium solution as all equilibrium points are unstable
-
-% ANSWER TO QUESTION 6 (along with figure 2)
+%%
+% Component Curves of Lotka-Volterra System
 figure(2)
-hold on 
+hold on
 plot(t_out, v_out(:,1))
 plot(t_out, v_out(:,2))
-legend('Deer Population', 'Mountain Lion Population')
+legend( 'Mountain Lion Population','Deer Population')
 title('Component Curves of Lotka-Volterra System')
 xlabel('Time')
 ylabel('Population in Dozens')
 hold off
-
-% The curves are out of phase, and this means that the interaction of the
-% predator and prey depends on the population of each other. If the deer
-% population is higher, it means the mountain lion population can rise
-% because they have more food. As they kill off more deer, there is less
-% food for them and their population goes down after the deer population
-% decreases. Then, when the mountain lion population is low, the deer
-% population can decrease because there are less predators to kill them,
-% and the cycle repeats.
--- Project01diff/project_1_2_6ab.m
+++ Project01diff/project_1_2_6ab.m
@@ -1,5 +1,4 @@
-% Section 2, question 6, part a, b, c
-
+% Eulers method with Harvest Factor, Section 2, Question 6
 r = .65;
 L = 8.1;
 p = 1.2;
@@ -28,15 +27,43 @@ end
 for n = 1:(length(t1)-1)
     x4(n+1) = x4(n) + h1*x_prime(t1(n),x4(n));
 end
+% dirfield.m
+%{
+function dirfield(f,tval,yval,plot_title)
+[tm,ym]=meshgrid(tval,yval);
+dt = tval(2) - tval(1); 
+dy = yval(2) - yval(1);
+fv = vectorize(f);
+if isa(f,'function_handle')
+  fv = eval(fv);
+end
+yp=feval(fv,tm,ym); 
+s = 1./max(1/dt,abs(yp)./dy)*0.35;
+h = ishold;
+quiver(tval,yval,s,s.*yp,0,'.r'); hold on;
+quiver(tval,yval,-s,-s.*yp,0,'.r');
+if h
+  hold on
+else
+  hold off
+end
+axis([tval(1)-dt/2,tval(end)+dt/2,yval(1)-dy/2,yval(end)+dy/2])
+
+title(plot_title);
+xlabel('t values');
+ylabel('y values');
+%}
 f = @(t,x) (0.65*(1-(x./8.1)).*x)-(1.2*x.^2./(1+x.^2));
 figure(1)
+% Plot of direction field and solutions
 hold on
 plot(t1,x1,'LineWidth', 1)
 plot(t1,x2,'LineWidth', 1)
 plot(t1,x3,'LineWidth', 1)
 plot(t1,x4,'LineWidth', 1)
+% Function call for dirfield.m
 dirfield(f,0:2:75, 0:0.2:10, 'Deer Pop. With Harvest')
 xlabel('time'), ylabel('Population in dozens') 
-
+legend('x_0 = 2', 'x_0 = 10', 'x_0 = 1.8', 'x_0 = 0.1')
 hold off
 
