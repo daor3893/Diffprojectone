17ad14382bab2566198ac1dd19db42772620d79a
Lilac Intrater <liin7533@colorado.edu>
2019-04-01 16:51:24 -0600

    orthog 3

---
+++ Project01diff/project 2/orthog3.m
@@ -0,0 +1,11 @@
+function[Sn]=orthog3(An)
+    v1 = An(:,1);
+    v2 = An(:,2);
+    v3 = An(:,3);
+    u1 = v1/norm(v1,2);
+    length1 = norm(u1,2);
+    u2 = v2-(u1*dot(u1,v2)/(length1).^2);
+    length2 =norm(u2,2);
+    u3 = v3-(u1*dot(u1,v3)/(length1).^2)-(u2*dot(u2,v3)/(length2).^2);
+    Sn=[u1 u2 u3];
+end
