8cc9a04145a021f6ef7d8f25616c098ebb0f6d01
Lilac Intrater <liin7533@colorado.edu>
2019-04-01 16:24:06 -0600

    Section 3 question3

---
+++ Project01diff/project 2/formAn.m
@@ -0,0 +1,10 @@
+function [An] = formAn(n)
+
+    n1=eye(n)*2;
+    v1=zeros(n-1,1);
+    positions = 1:n-1;
+    v1(positions) = -1;
+    n2=diag(v1, 1);
+    n3=diag(v1,-1);
+    An=n1+n2+n3;
+end
