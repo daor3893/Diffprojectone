035fe72971cb1b76e82c84edb9b25c08ad12d070
Lilac Intrater <liin7533@colorado.edu>
2019-04-02 11:11:24 -0600

    yep

--- Project01diff/project 2/orthog3.m
+++ Project01diff/project 2/orthog3.m
@@ -5,7 +5,9 @@ function[Sn]=orthog3(An)
     u1 = v1/norm(v1,2);
     length1 = norm(u1,2);
     u2 = v2-(u1*dot(u1,v2)/(length1).^2);
+    u2 = u2/norm(u2);
     length2 =norm(u2,2);
     u3 = v3-(u1*dot(u1,v3)/(length1).^2)-(u2*dot(u2,v3)/(length2).^2);
+    u3 = u3/norm(u3);
     Sn=[u1 u2 u3];
 end
--- Project01diff/project 2/test.m
+++ Project01diff/project 2/test.m
@@ -8,3 +8,9 @@ Sn=orthog3(An);
 disp(dot(u1,u2))
 disp(dot(u2,u3))
 disp(dot(u3,u1))
+disp(norm(u1))
+disp(norm(u2))
+disp(norm(u3))
+% The dot product between each vector is very small (close to zero), so
+% therefore the vectors are orthogonal. Also, all the vectors are unit
+% vectors, because their lengths are all 1
